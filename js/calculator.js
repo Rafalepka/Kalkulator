429e9a8f9994c96f28bbe9cafc7be853ea35d662
Rafal Matalowski <rafal.matalowski@gmail.com>
2016-12-04 17:02:04 +0100

    init
    Calculator

---
+++ js/calculator.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+function inputClick(value) {
+    var viewValue = document.getElementById('view').value;
+
+    if (value != 'AC' && value != '=' && value != '.') { // sprawdzenie znakow, ktore sa rozne od AC . i = 
+        document.getElementById('view').value += value;
+    } else if (value == 'AC') {
+        document.getElementById('view').value = '';
+    } else if (value == '.') {
+        if (viewValue.slice(-1) != ".") {
+            document.getElementById('view').value += value;
+        }
+    } else {
+        if (isNaN(viewValue.slice(-1)) || viewValue.indexOf('/0')>-1){
+            document.getElementById('view').value = 'Error';
+        }
+        
+        else 
+        
+        {
+            document.getElementById('view').value = eval(viewValue);
+        }
+    }
+}
+
+
+        //    var result = eval(viewValue);
+
