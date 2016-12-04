f4409580e8583c25dd91990e958fb8ea8cfe4b14
Rafal Matalowski <rafal.matalowski@gmail.com>
2016-12-04 17:05:31 +0100

    komentarze

--- js/calculator.js
+++ js/calculator.js
@@ -5,26 +5,23 @@ function inputClick(value) {
     var viewValue = document.getElementById('view').value;
 
     if (value != 'AC' && value != '=' && value != '.') { // sprawdzenie znakow, ktore sa rozne od AC . i = 
-        document.getElementById('view').value += value;
+        document.getElementById('view').value += value; // w poluu wprowadzania dodaje kolejne liczby i znaki dzialania
     } else if (value == 'AC') {
         document.getElementById('view').value = '';
     } else if (value == '.') {
-        if (viewValue.slice(-1) != ".") {
+        if (viewValue.slice(-1) != ".") { // sprawdza czy ostatni elelement jest rozny od kropki
             document.getElementById('view').value += value;
         }
-    } else {
-        if (isNaN(viewValue.slice(-1)) || viewValue.indexOf('/0')>-1){
-            document.getElementById('view').value = 'Error';
-        }
-        
-        else 
-        
+    } else { //jesli klikniemy przycisk
+        if (isNaN(viewValue.slice(-1)) || viewValue.indexOf('/0') > -1) { //kiedy jest liczba lub kiedy w ciagu znakow jest /0
+            document.getElementById('view').value = 'Error'; // wyswietl error
+        } else
+
         {
-            document.getElementById('view').value = eval(viewValue);
+            document.getElementById('view').value = eval(viewValue); // wylicza wynik dzialania i wyswietla w polu wynik
         }
     }
 }
 
 
-        //    var result = eval(viewValue);
-
+//    var result = eval(viewValue);
